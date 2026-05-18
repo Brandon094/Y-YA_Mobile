0ce832e535e0f2d11816e6dfdabfd99d0689f001
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-05-18 13:18:26 -0500

    Refactor: Acctualizacion de importaciones para usar el modelo

--- app/src/main/java/com/example/myapplication/ui/screens/edit_profile/EditProfileViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/edit_profile/EditProfileViewModel.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myapplication.data.SupabaseManager
-import com.example.myapplication.ui.screens.profile.UserProfile
+import com.example.myapplication.data.models.UserProfile
 import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.postgrest.postgrest
 import kotlinx.coroutines.launch
